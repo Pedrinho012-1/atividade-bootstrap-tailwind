38e7938fa89cc2c0f27edac6b08e8d317d56a625
Pedrinho012-1 <pedropaulogoncalves58@gmail.com>
2025-04-30 03:21:36 -0300

    Add files via upload

---
+++ atividade-bootstrap-tailwind/js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentloaded",  () => {
+    console.log("site carregado!");
+
+const SaibaMaisbtn = document.querySelector("[data-bs-target='#infocurso']");
+if (saibamaisbtn) {
+    SaibaMaisbtn.addEventListener("click", () => {
+        console.log("Botão 'Saiba mais' clicado!");
+   });
+  }
+});
